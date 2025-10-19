a2d485a9352412eb51e6403441d84e741dc0cc33
Leechungwoon <dlcjddns22@gmail.com>
2025-10-19 22:48:18 +0900

    계산기 (App,lv2)

--- src/project/calculator/App.java
+++ src/project/calculator/App.java
@@ -1,11 +1,14 @@
 package project.calculator;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class App {
+public class App extends Calculator {
     public static void main(String[] args) {
         //1. 입력 스캐너 생성
         Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
 
         //2. 숫자 입력 지정
         while (true) {
@@ -16,37 +19,20 @@ public class App {
 
             // 3. 사칙연산 기호 입력
             System.out.print("사칙연산을 입력하세요: ");
-            char c = scanner.next().charAt(0);
+            char operator = scanner.next().charAt(0);
+
+            int result = calculator.calculate(num1, num2, operator);
 
             //4. 결과값 변수 지정
-            int result = 0;
+//            int result = 0;
 
-            //5. 계산하기
-            switch (c) {
-                case '+':
-                    System.out.println(result = num1 + num2);
-                    break;
-                case '-':
-                    System.out.println(result = num1 - num2);
-                    break;
-                case '*':
-                    System.out.println(result = num1 * num2);
-                    break;
-                case '/':
-                    if (num2 != 0) {
-                        System.out.println(result = num1 / num2);
-                    } else {
-                        System.out.println("분모는 0이 될 수 없습니다.");
-                    }
-                default:
-                    System.out.println("결과값: " + result);
-                    // 반복되게 시도
-//                    continue;
-            }
             //6. 계산 결과 출력
+            //TODO 2. ArrayList 받을 예정
+//            calculator.getArrayList(result );
+//            System.out.println(arrayList);
             System.out.println("결과: " + result);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            // 7. 입력값 받는 부분
+            // 7. 입력값 받는 부분 -> null 안정성 참고
             String a = scanner.next();
             // 8. 비교
             if (a.equals("exit")) {
@@ -58,5 +44,4 @@ public class App {
         }
     }
 }
-
 
