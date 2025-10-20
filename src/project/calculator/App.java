c224cd2c452aa648fc76b9478637627c5c944a63
Leechungwoon <dlcjddns22@gmail.com>
2025-10-20 14:06:39 +0900

    계산기 (app, 데이터저장(setter) 데이터 조회(getter)

--- src/project/calculator/App.java
+++ src/project/calculator/App.java
@@ -1,5 +1,6 @@
 package project.calculator;
 
+import javax.lang.model.type.ArrayType;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -21,20 +22,25 @@ public class App extends Calculator {
             System.out.print("사칙연산을 입력하세요: ");
             char operator = scanner.next().charAt(0);
 
-            int result = calculator.calculate(num1, num2, operator);
-
             //4. 결과값 변수 지정
+            int result = calculator.calculate(num1, num2, operator);
 //            int result = 0;
 
-            //6. 계산 결과 출력
             //TODO 2. ArrayList 받을 예정
-            //객체 생성
-            Calculator.addResult(result);
+            //3-1) 결과 출력
+            calculator.addResult(result);
 
+            //4-1) getter 활용
+            ArrayList<Integer> getList = calculator.getArrayList();
+            System.out.println("결과 저장: " + getList);
+
+            //6. 계산 결과 출력
             System.out.println("결과: " + result);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+
             // 7. 입력값 받는 부분 -> null 안정성 참고
             String a = scanner.next();
+
             // 8. 비교
             if (a.equals("exit")) {
                 System.out.println("계산을 종료합니다.");
