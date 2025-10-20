51399d0701d26c885d216ed2355d9973be648015
Leechungwoon <dlcjddns22@gmail.com>
2025-10-20 14:07:19 +0900

    계산기 (Calculator, 데이터저장(setter) 데이터 조회(getter)

--- src/project/calculator/Calculator.java
+++ src/project/calculator/Calculator.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Calculator {
     // 연산 결과 저장하는 컬렉션 타입 필드 생성
     //1. 속성
+    //(순서: 2) 컬렉션 선언
     private static ArrayList<Integer> arrayList = new ArrayList<>();
-    int result;
 
 
     //2. 생성자
@@ -16,15 +16,20 @@ public class Calculator {
     // 여러 개가 존재할 수 있다.
 
     //3. 기능
-    //TODO 1. ArrayList 결과 값을 저장하는 얻는다.
-    //결과값 저장하는 메서드
-     public static void addResult(int result) {
+
+    //TODO 1. ArrayList 결과 값을 저장하는 값을 얻는다.
+    //(순서: 3번)결과값 저장하는 메서드 -> 저장하는 것은 setter
+     public void addResult(int result) {
          arrayList.add(result); //result 데이터 추가
      }
 
-     //getter
+    //(순서: 4번)getter -> 조회
+     //결과 꺼내오기
+     public ArrayList<Integer> getArrayList() {
+         return arrayList; //데이터 찾기
+     }
 
-    // 1. 사칙연산을 수행한 후, 결과값을 반환하는 매서드 구헌
+    // (순서: 1번) 사칙연산을 수행한 후, 결과값을 반환하는 매서드 구헌
     //접근제어자, 반환데이터, 변수이름, 매개변수
     public int calculate (int num1, int num2, char operator) {
         int result = 0;
